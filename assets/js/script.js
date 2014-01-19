2c86b1f306e159409510c7da7c83b1b5bccecbea
Thomas Clausen <kontakt@thomasclausen.dk>
2014-01-19 12:11:46 +0100

    Check if image actually exsists and other minor changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,25 +8,27 @@
 	$(document).ready(function(){
 		$('html').removeClass('no-js');
 
-		if ($('body').hasClass('home-template')) {
+		if ($('body').hasClass('home-template') || $('body').hasClass('archive-template')) {
 			$('article').each(function() {
-				$('.post-image .rectangle .circle', this).prepend('<div class="image" />');
-				$('.post-image .rectangle .circle .image', this).css({
-					 backgroundImage: 'url(' + $('.post-image .rectangle .circle img', this).attr('src') + ')'
-				});
-				$('.post-image .rectangle .circle p', this).remove();
+				if ($('.post-image .rectangle .circle img', this).attr('src') != undefined) {
+					$('.post-image .rectangle .circle', this).prepend('<div class="image" />');
+					$('.post-image .rectangle .circle .image', this).css({
+						 backgroundImage: 'url(' + $('.post-image .rectangle .circle img', this).attr('src') + ')'
+					});
+					$('.post-image .rectangle .circle p', this).remove();
+				}
 			});
-			$('article .post-image').on('click', function() {
+			/*$('article .post-image').on('click', function() {
 				$(this).parent().find('.post-content a').attr('href');
-			});
+			});*/
 		}
 
 		if ($('body').hasClass('post-template')) {
 			$('article .post-content p:first-of-type img:first-of-type').prependTo('article .post-image');
-
+			if ($('article .post-content p:first-of-type').html() == '') {
+				$('article .post-content p:first-of-type').remove();
+			}
 			$('article .post-content .post-meta time').html($('article .post-content .post-meta time').text().substr(0, 2) + '<sup>' + $('article .post-content .post-meta time').text().substr(2, 2) + '</sup>' + $('article .post-content .post-meta time').text().substr(5));
-
-			s.substring(0, s.indexOf('?'))
 		}
 	});
 }(jQuery));
