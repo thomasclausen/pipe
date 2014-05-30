28a5cfe04121e95f814b6c0e2ee3b4c4055826f6
Thomas Clausen <kontakt@thomasclausen.dk>
2014-05-30 12:13:49 +0200

    Update gulp destination folder

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,16 @@ var gulp = require('gulp'),
     pkg = require('./package.json');
 
 var paths = {
+  styles: [
+    'src/assets/css/normalize.css',
+    'src/assets/css/style.css'
+  ],
+  scripts: [
+    'src/assets/js/jquery.fitvids.js',
+    'src/assets/js/script.js',
+  ],
+  images: 'src/assets/images/*',
+
   copy: {
     root: [
       'src/*.hbs',
@@ -28,20 +38,11 @@ var paths = {
       'src/assets/js/html5shiv.js',
       'src/assets/js/respond.min.js'
     ]
-  },
-  styles: [
-    'src/assets/css/normalize.css',
-    'src/assets/css/style.css'
-  ],
-  scripts: [
-    'src/assets/js/jquery.fitvids.js',
-    'src/assets/js/script.js',
-  ],
-  images: 'src/assets/images/*'
+  }
 };
 
 gulp.task('clean', function() {
-  return gulp.src('dist', {read: false})
+  return gulp.src(pkg.name, {read: false})
     .pipe(clean());
 });
 
@@ -50,7 +51,7 @@ gulp.task('styles', function() {
     .pipe(concat(pkg.name + '.css'))
     .pipe(cssmin())
     .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('dist/assets/css'))
+    .pipe(gulp.dest(pkg.name + '/assets/css'))
     .pipe(notify({ message: 'Styles task complete' }));
 });
 gulp.task('styles-test', function() {
@@ -64,7 +65,7 @@ gulp.task('scripts', function() {
     .pipe(concat(pkg.name + '.js', {newLine: ';'}))
     .pipe(uglify())
     .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('dist/assets/js'))
+    .pipe(gulp.dest(pkg.name + '/assets/js'))
     .pipe(notify({ message: 'Scripts task complete' }));
 });
 gulp.task('scripts-test', function() {
@@ -76,24 +77,24 @@ gulp.task('scripts-test', function() {
 gulp.task('images', function() {
   return gulp.src(paths.images)
     .pipe(imagemin({optimizationLevel: 3, progressive: true, interlaced: true}))
-    .pipe(gulp.dest('dist/assets/images'))
+    .pipe(gulp.dest(pkg.name + '/assets/images'))
     .pipe(notify({ message: 'Images task complete' }));
 });
 
 gulp.task('copy', function() {
   gulp.src(paths.copy.root)
-    .pipe(gulp.dest('dist/'));
+    .pipe(gulp.dest(pkg.name));
   gulp.src(paths.copy.partials_root)
-    .pipe(gulp.dest('dist/partials/'));
+    .pipe(gulp.dest(pkg.name + '/partials/'));
   gulp.src(paths.copy.partials_custom)
-    .pipe(gulp.dest('dist/partials/custom/'));
+    .pipe(gulp.dest(pkg.name + '/partials/custom/'));
   gulp.src(paths.copy.fonts)
-    .pipe(gulp.dest('dist/assets/fonts/'));
+    .pipe(gulp.dest(pkg.name + '/assets/fonts/'));
   gulp.src(paths.copy.js)
-    .pipe(gulp.dest('dist/assets/js/'));
+    .pipe(gulp.dest(pkg.name + '/assets/js/'));
 });
 
-gulp.task('watch', function() {
+gulp.task('wath', function() {
   gulp.watch(paths.styles, ['styles-test', 'styles']);
   gulp.watch(paths.scripts, ['scripts-test', 'scripts']);
   gulp.watch(paths.images, ['images']);
@@ -113,13 +114,13 @@ gulp.task('test', function() {
 });
 
 gulp.task('zip', function() {
-  gulp.src('**', { cwd: path.join(process.cwd(), 'dist')})
+  gulp.src('**', { cwd: path.join(process.cwd(), pkg.name)})
     .pipe(zip(pkg.name + '-ghost.zip'))
     .pipe(gulp.dest('.'));
 });
 
 gulp.task('upload', function () {
-  gulp.src('**', { cwd: path.join(process.cwd(), 'dist')})
+  gulp.src('**', { cwd: path.join(process.cwd(), pkg.name)})
     .pipe(ftp({
       host: 'website.com',
       user: 'johndoe',
