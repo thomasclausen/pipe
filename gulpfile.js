ecdc86711cb1bdb50f7bc93606c0eaeef5ba1b2b
Thomas Clausen <kontakt@thomasclausen.dk>
2014-10-08 21:19:18 +0200

    Gettin' SASSY

--- gulpfile.js
+++ gulpfile.js
@@ -4,23 +4,28 @@ var gulp = require('gulp'),
     concat = require('gulp-concat'),
     rename = require('gulp-rename'),
     htmlreplace = require('gulp-html-replace'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
     csslint = require('gulp-csslint'),
     cssmin = require('gulp-cssmin'),
+    uncss = require('gulp-uncss'),
     jshint = require('gulp-jshint'),
     jsmin = require('gulp-uglify'),
     imagemin = require('gulp-imagemin'),
-    notify = require('gulp-notify'),
     watch = require('gulp-watch'),
     zip = require('gulp-zip'),
     ftp = require('gulp-ftp'),
+    psi = require('psi'),
     pkg = require('./package.json');
 
 var paths = {
   styles: [
     'src/assets/css/normalize.css',
-    'src/assets/css/style.css'
+    'src/assets/css/style.scss'
   ],
   scripts: [
+    'src/assets/js/classie.js',
+    'src/assets/js/imagesloaded.pkgd.min.js',
     'src/assets/js/jquery.fitvids.js',
     'src/assets/js/script.js',
   ],
@@ -31,14 +36,12 @@ var paths = {
       'src/*.hbs',
       'src/*.json'
     ],
-    partials_root: 'src/partials/*.hbs',
-    partials_custom: 'src/partials/custom/*.hbs',
     fonts: 'src/assets/fonts/*',
     js: [
-      'src/assets/js/jquery-1.10.2.min.js',
-      'src/assets/js/html5shiv.js',
-      'src/assets/js/respond.min.js'
-    ]
+      'src/assets/js/jquery-1.10.2.min.js'
+    ],
+    partials_root: 'src/partials/*.hbs',
+    partials_custom: 'src/partials/custom/*.hbs'
   }
 };
 
@@ -49,15 +52,16 @@ gulp.task('clean', function() {
 
 gulp.task('styles', function() {
   return gulp.src(paths.styles)
+    .pipe(sass({ style: 'compressed' }))
+    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
     .pipe(concat(pkg.name + '.css'))
     .pipe(cssmin())
     .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest(pkg.name + '/assets/css'))
-    .pipe(notify({ message: 'Styles task complete' }));
+    .pipe(gulp.dest(pkg.name + '/assets/css'));
 });
-gulp.task('styles-test', function() {
-  return gulp.src(paths.styles)
-    .pipe(csslint())
+gulp.task('styles-test', ['styles'], function() {
+  return gulp.src(pkg.name + '/css/style.css')
+    .pipe(csslint('csslintrc.json'))
     .pipe(csslint.reporter());
 });
 
@@ -66,53 +70,57 @@ gulp.task('scripts', function() {
     .pipe(concat(pkg.name + '.js', {newLine: ';'}))
     .pipe(jsmin())
     .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest(pkg.name + '/assets/js'))
-    .pipe(notify({ message: 'Scripts task complete' }));
+    .pipe(gulp.dest(pkg.name + '/assets/js'));
 });
-gulp.task('scripts-test', function() {
-  return gulp.src(paths.scripts)
-    .pipe(jshint())
+gulp.task('scripts-test', ['scripts'], function() {
+  return gulp.src('src/assets/js/script.js')
+    .pipe(jshint('jshintrc.json'))
     .pipe(jshint.reporter());
 });
 
 gulp.task('images', function() {
   return gulp.src(paths.images)
     .pipe(imagemin({optimizationLevel: 3, progressive: true, interlaced: true}))
-    .pipe(gulp.dest(pkg.name + '/assets/images'))
-    .pipe(notify({ message: 'Images task complete' }));
+    .pipe(gulp.dest(pkg.name + '/assets/images'));
 });
 
 gulp.task('copy', function() {
   gulp.src(paths.copy.root)
     .pipe(gulp.dest(pkg.name));
-  gulp.src(paths.copy.partials_root)
-    .pipe(gulp.dest(pkg.name + '/partials/'));
-  gulp.src(paths.copy.partials_custom)
-    .pipe(gulp.dest(pkg.name + '/partials/custom/'));
   gulp.src(paths.copy.fonts)
     .pipe(gulp.dest(pkg.name + '/assets/fonts/'));
   gulp.src(paths.copy.js)
     .pipe(gulp.dest(pkg.name + '/assets/js/'));
+  gulp.src(paths.copy.partials_root)
+    .pipe(gulp.dest(pkg.name + '/partials/'));
+  gulp.src(paths.copy.partials_custom)
+    .pipe(gulp.dest(pkg.name + '/partials/custom/'));
 });
 
 gulp.task('replace', ['copy'], function() {
   gulp.src('src/default.hbs')
     .pipe(htmlreplace({
-      'css': '<link rel="stylesheet" id="' + pkg.name + '-css" href="{{asset "css/' + pkg.name + '.min.css"}}" />',
-      'js': '<script src="{{asset "js/' + pkg.name + '.min.js"}}" async></script>'
+      css: {
+        src: '{{asset "css/' + pkg.name + '.min.css"}}',
+        tpl: '<link rel="stylesheet" id="' + pkg.name + '-css" href="%s" />'
+      },
+      js: {
+        src: '{{asset "js/' + pkg.name + '.min.js"}}',
+        tpl: '<script src="%s" async></script>'
+      }
     }))
     .pipe(gulp.dest(pkg.name));
 });
 
 gulp.task('wath', function() {
-  gulp.watch(paths.styles, ['styles-test', 'styles']);
-  gulp.watch(paths.scripts, ['scripts-test', 'scripts']);
+  gulp.watch(paths.styles, ['styles']);
+  gulp.watch(paths.scripts, ['scripts']);
   gulp.watch(paths.images, ['images']);
   gulp.watch(paths.copy.root, ['replace']);
-  gulp.watch(paths.copy.partials_root, ['replace']);
-  gulp.watch(paths.copy.partials_custom, ['replace']);
   gulp.watch(paths.copy.fonts, ['replace']);
   gulp.watch(paths.copy.js, ['replace']);
+  gulp.watch(paths.copy.partials_root, ['replace']);
+  gulp.watch(paths.copy.partials_custom, ['replace']);
 });
 
 gulp.task('default', ['clean'], function() {
@@ -137,4 +145,22 @@ gulp.task('upload', function () {
       pass: '1234',
       remotePath: '/html/'
     }));
+});
+
+gulp.task('psi-mobile', function (cb) {
+  psi({
+    nokey: 'true',
+    url: 'http://user.ghost.io',
+    locale: 'da_DK',
+    strategy: 'mobile',
+    threshold: 80
+  }, cb);
+});
+gulp.task('psi-desktop', function (cb) {
+  psi({
+    nokey: 'true',
+    url: 'http://user.ghost.io',
+    locale: 'da_DK',
+    threshold: 80
+  }, cb);
 });
